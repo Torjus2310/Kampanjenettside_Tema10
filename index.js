4f2ed4689220f878df081217de4aa170b8094a78
Torjus2310 <56019539+Torjus2310@users.noreply.github.com>
2020-10-16 17:01:28 +0200

    add script

---
+++ index.js
@@ -0,0 +1,31 @@
+let textIn = document.querySelector ('.textIn');
+let textIn2 = document.querySelector ('.textIn2');
+let h = window.innerHeight;
+let scrollPos = window.scrollY;
+
+console.log('height',h)
+console.log(scrollPos)
+
+
+window.on
+
+/*
+window.addEventListener('scroll', () => {
+
+        textIn.classList.add ('fade-in-element')
+    
+               
+}
+})
+
+window.addEventListener('scroll', () => {
+    console.log(window.scrollY)
+    
+    if ( scrollY < 1300) {
+        
+        textIn2.classList.add ('fade-in-element')
+               
+}
+})*/
+
+
