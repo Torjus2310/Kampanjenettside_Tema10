232c123623dfd4f98a45b44e762f18e8cc70065f
Ludvik <51988622+Ludkva@users.noreply.github.com>
2020-10-16 18:18:40 +0200

    Scroll animation on bottle -START-

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ let textIn = document.querySelector ('.textIn');
 let textIn2 = document.querySelector ('.textIn2');
 let h = window.innerHeight;
 let scrollPos = window.scrollY;
+let bottle = document.querySelector('.bottle');
+let frame = 0;
 
 console.log('height',h)
 console.log(scrollPos)
 
 
 window.on
+
+
+window.addEventListener('scroll', (event)=>{
+        if(scrollY > scrollPos){
+                console.log(scrollY)
+
+                bottle.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -310px);"
+                frame += .25;
+                if(frame > 8) frame = 0  
+        }
+   
+    })
+
+
+
+
 
 /*
 window.addEventListener('scroll', () => {
